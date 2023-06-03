8ab63596cd29e1346cbaa3dd889447f5532c4a1a
Doonguin <penguinicesp@gmail.com>
2023-06-03 11:07:07 +0200

    auto backup original files when path is selected

--- lib/commands.py
+++ lib/commands.py
@@ -1,25 +1,35 @@
 # Imports
 from tkinter import filedialog, messagebox
+import shutil
 import os
 
 # Global vars
 filename = "Hamsterball.exe"
+backedup = "textures/backedup"
+ballfiles = ["BallBorder.png", "HamsterBall.png", "HamsterBall-Mip1.png", "HamsterBall-Mip2.png", "HamsterBall-Mip3.png"]
 
 # Commands
 def fileSelect(target):
     loc = filedialog.askdirectory()
 
-    filepath = os.path.join(loc, filename)
-
-    if (os.path.isfile(filepath)):
-        try:
-            os.mkdir(os.path.join(loc, "textures/backedup"))
-        except:
-            messagebox.showerror("No permission", "Program doesn't have permission to change files in this directory. Quick fix: Run as administrator")
+    if loc == "":
+            loc = "[not yet selected]"
             return
 
-        if loc == "":
-            loc = "[not yet selected]"
+    filepath = os.path.join(loc, filename)
+    textures = os.path.join(loc, "textures")
+    dirpath = os.path.join(loc, backedup)
+
+    if (os.path.isfile(filepath)):
+        if (not os.path.isdir(dirpath)):
+            try:
+                os.mkdir(dirpath)
+
+                for img in ballfiles:
+                    shutil.copy2(os.path.join(textures, img), dirpath)
+            except:
+                messagebox.showerror("No permission", "Program doesn't have permission to change files in this directory. Quick fix: Run as administrator")
+                return
 
         target.configure(
                         text=loc
